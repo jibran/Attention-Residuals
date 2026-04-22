df349c91200a50f17094aef6a8384fb119c80579
Jibran Ijaz <jibran.ijaz@gmail.com>
2026-04-22 17:50:07 -0400

    Minor updates.
    Signed-off-by: Jibran Ijaz <jibran.ijaz@gmail.com>

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -2,10 +2,14 @@
 
 from dataset.base_dataset import BaseDataset
 from dataset.image_datasets import CIFAR10Dataset, MNISTDataset, get_dataset_class
+from dataset.shakespeare_dataset import ShakespeareDataset
+from dataset.tinystories_dataset import TinyStoriesDataset
 
 __all__ = [
     "BaseDataset",
     "MNISTDataset",
     "CIFAR10Dataset",
     "get_dataset_class",
+    "ShakespeareDataset",
+    "TinyStoriesDataset",
 ]
